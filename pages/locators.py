571181e1f1737ada522fa4a94895199d54a8108d
tbrumble <tbrumble@yandex.ru>
2020-05-11 11:46:55 +0300

    Some refactor

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_FORM = '#content_inner > div > div.col-sm-6.login_form'
     REGISTER_FROM = '#content_inner > div > div.col-sm-6.register_form'
-
