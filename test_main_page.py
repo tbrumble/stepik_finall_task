0f46b1f4583550fc487ebe11cf16a76f34382460
tbrumble <tbrumble@yandex.ru>
2020-05-11 11:45:36 +0300

    locators.py: -> Added new constants to login page
    login_page.py:
    -> Added new methods to find login form, registration form, and check current urls in login page
    
    test_main_page.py:
    -> Refactor to check login_page.py work

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,13 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    # link = "http://selenium1py.pythonanywhere.com/"
+    # page = MainPage(browser, link)
+    # page.open()
+    # page.should_be_login_link()
 
+    page1 = LoginPage(browser, "http://selenium1py.pythonanywhere.com/ru/accounts/login/")
+    page1.open()
+    page1.should_be_login_url()
